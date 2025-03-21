0473b68f3db598c5f5e074e8e37a9ec93752fe7e
DELL <DELL@DESKTOP-GQBJEDV>
2025-03-21 12:10:37 +0530

    Added webform

--- LibrarySystem/Properties/WebForm1.aspx.cs
+++ LibrarySystem/Properties/WebForm1.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace LibrarySystem
+namespace LibrarySystem.Properties
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
--- LibrarySystem/chjng.aspx.designer.cs
+++ LibrarySystem/chjng.aspx.designer.cs
@@ -10,7 +10,16 @@
 
 namespace LibrarySystem
 {
-    public partial class WebForm1
+    public partial class chjng
     {
+
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }
